b9986c8ebe9aad8b0ea4fea63847b1a07ce4d6e9
Jonathan Kerr <jonathandrewkerr@gmail.com>
2020-07-01 22:41:57 +0100

    Add the problem statement
    I'm really not happy with the content on this page - I feel like I need to bring in an example to explain why I'm calculating all possible scores.

--- app/pitch/routes.py
+++ app/pitch/routes.py
@@ -6,3 +6,8 @@ from app.pitch import pitch_bp
 @pitch_bp.route("/start", methods=["GET"])
 def start():
     return render_template("pitch/start.html")
+
+
+@pitch_bp.route("/the-problem", methods=["GET"])
+def problem():
+    return render_template("pitch/the-problem.html")
