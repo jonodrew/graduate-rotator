beab15b7031d2498a5965b58e89b7c34dda55115
Jonathan Kerr <jonathandrewkerr@gmail.com>
2020-07-03 21:32:41 +0100

    Set up the solution
    Pass the matrix to the munkres API, solve it, then pass back a nice list of coordinates. Turn this back into human-readable language and present it to the user.

--- app/pitch/routes.py
+++ app/pitch/routes.py
@@ -1,5 +1,7 @@
-from flask import render_template, session
+import json
+from flask import render_template, session, request, url_for
 from app.pitch import pitch_bp
+import requests
 from random import randrange
 
 
@@ -18,3 +20,20 @@ def approach():
     semi_random_data = [[randrange(0, 25) for i in range(10)] for j in range(10)]
     session["matrix"] = semi_random_data
     return render_template("pitch/the-approach.html", data=semi_random_data)
+
+
+@pitch_bp.route("/the-solution", methods=["POST"])
+def solution():
+    if request.method == "POST":
+        result = requests.post(
+            url_for("munkres_api.process_matrix", _external=True),
+            data=json.dumps({"matrix": session["matrix"]}),
+        )
+        return render_template(
+            "pitch/the-solution.html",
+            indexes=json.loads(result.content),
+            data=session["matrix"],
+            lookup={i: chr(i + 65) for i in range(10)},
+        )
+    else:
+        pass
