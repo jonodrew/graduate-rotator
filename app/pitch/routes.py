e9c8cf5ef43ab4d6d712b50749c53fb256d88893
Jonathan Kerr <jonathandrewkerr@gmail.com>
2020-07-03 21:36:06 +0100

    Show users a random selection of data
    The aim of this screen is to demonstrate how complex even a small task in this space is

--- app/pitch/routes.py
+++ app/pitch/routes.py
@@ -1,6 +1,6 @@
-from flask import render_template
-
+from flask import render_template, session
 from app.pitch import pitch_bp
+from random import randrange
 
 
 @pitch_bp.route("/start", methods=["GET"])
@@ -11,3 +11,10 @@ def start():
 @pitch_bp.route("/the-problem", methods=["GET"])
 def problem():
     return render_template("pitch/the-problem.html")
+
+
+@pitch_bp.route("/the-approach", methods=["GET"])
+def approach():
+    semi_random_data = [[randrange(0, 25) for i in range(10)] for j in range(10)]
+    session["matrix"] = semi_random_data
+    return render_template("pitch/the-approach.html", data=semi_random_data)
